76997b2fd744ec544dce72178df7dfc4529e81a9
restufff <restuff666@gmail.com>
2021-06-18 19:49:51 +0700

    initial commit

---
+++ src/assets/page/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../parts/Header";
+import Hero from "../parts/Hero";
+
+function HomePage(props) {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+export default HomePage;
