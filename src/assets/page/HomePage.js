2c91534c39b8e858526a2ac34befbdbd631af8f8
restufff <restuff666@gmail.com>
2021-06-19 16:54:52 +0700

    adding sitemap and clients page

--- src/assets/page/HomePage.js
+++ src/assets/page/HomePage.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
+import BrowseRoom from "../parts/HomePage/BrowseRoom";
 import JustArrived from "../parts/HomePage/JustArrived";
+import Sitemap from "../parts/HomePage/Sitemap";
+import Clients from "../parts/Clients";
 
 function HomePage(props) {
   return (
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
       <JustArrived />
+      <Clients />
+      <Sitemap />
     </>
   );
 }
