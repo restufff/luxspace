34d9019011ec5d0aa70a493c877c119c3a3aba29
restufff <restuff666@gmail.com>
2021-06-24 17:31:20 +0700

    adding custom hooks (useScrollModal and useScrollDOM)

--- src/assets/page/HomePage.js
+++ src/assets/page/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "../parts/Header";
-import Hero from "../parts/Hero";
+import Hero from "../parts/HomePage/Hero";
 import BrowseRoom from "../parts/HomePage/BrowseRoom";
 import JustArrived from "../parts/HomePage/JustArrived";
 import Sitemap from "../parts/HomePage/Sitemap";
 import Clients from "../parts/Clients";
 import Footer from "../parts/Footer";
+import useScrollAcnhor from "../helpers/hooks/useScrollAcnhor";
+import useModalDOM from "../helpers/hooks/useModalDOM";
 
 function HomePage(props) {
+  useScrollAcnhor();
+  useModalDOM();
   return (
     <>
       <Header theme="white" position="absolute" />
